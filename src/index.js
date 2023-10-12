b76ff14d069c306b452440bdcd4ecd34f7bee9aa
Yan <young69budda@gmail.com>
2023-10-12 03:26:45 +0300

    gh

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { FavoritesProvider } from "./hooks/FavoritesContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router basename="/home">
+  <Router basename="/">
     <FavoritesProvider>
       <App />
     </FavoritesProvider>
